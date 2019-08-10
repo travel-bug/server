53af048dfe0044bee0306ea4d24348f2bb01a036
Mike Z <mzapata2991@gmail.com>
2019-08-10 16:24:41 -0500

    got stuff talking

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("./node_modules/express");
-const mongoose = require("mongoose");
-const passport = require("passport");
+const passport = require("./passport");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -9,21 +8,13 @@ const PORT = process.env.PORT || 3001;
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(passport.initialize());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 // Add routes, both API and view
 require("./routes/routes")(app, passport);
-
-// Connect to the Mongo DB
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
-  {
-    useCreateIndex: true,
-    useNewUrlParser: true
-  }
-);
 
 // Start the API server
 app.listen(PORT, () =>

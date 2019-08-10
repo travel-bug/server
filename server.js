2669dd033003743365a2fa7af6b75504bc39c020
Mike Z <mzapata2991@gmail.com>
2019-08-10 16:32:07 -0500

    added cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("./node_modules/express");
 const passport = require("./passport");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(passport.initialize());
+app.use(cors());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));

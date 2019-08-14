1cc8b7255056defbb63ad46e1276dae77e9816ac
Anne <Coleman>
2019-08-13 19:17:38 -0500

    orm

--- orm.js
+++ orm.js
@@ -1,13 +1,66 @@
-var connection = require ("./connection.js");
+let connection = require('./connection');
 
-var orm = {
-    select: function(queryObject, callback){
-        let queryString ="SELECT ?? FROM ??";
+let orm = {
+    
+    select: function(query, callback) {
+        let queryString = "SELECT ?? FROM ??";
         let searchCriteria = [query.columns || ['*'], query.table];
-        let queryConnection = this._buildTableQuery(queryString, searchCriteria, queryConnection);
-     {  
-         let statement = connection.query(queryString, searchCriteria, function(error,result){
-             
-         })
+        if (query.where){
+            queryString = orm._buildWhereStatement(query, queryString, searchCriteria);
+        }
+        let statement = connection.query(queryString, searchCriteria, function(error, result) {
+            callback(error, result);
+        });
+        if (query.debug){
+            console.log(statement.sql);
+        }
+    },
+    _buildWhereStatement: function(query, queryString, searchCriteria){
+        queryString += " WHERE ";
+        let whereString = [];
+        for (let where in query.where) {
+            searchCriteria.push(query.where[where]);
+            whereString.push(' ? ');
+        }
+        let operator = query.operator || 'AND';
+        queryString += whereString.join(operator);
+        return queryString;
+    },
+    insert: function(query, callback) {
+        let queryString = "INSERT INTO ?? SET ?";
+        let statement = connection.query(queryString, [query.table, query.data], function(error, result) {
+            callback(error, result);
+        });
+        if (query.debug){
+            console.log(statement.sql);
+        }
+    },
+    update: function(query, callback) {
+        let queryString = "UPDATE ?? SET ? WHERE ?";
+        let statement = connection.query(queryString, [query.table, query.data, query.where[0]], function(error, result) {
+            callback(error, result);
+        });
+        if (query.debug){
+            console.log(statement.sql);
+        }
+    },
+    delete: function(query, callback) {
+        let queryString = "DELETE FROM ?? WHERE ?";
+        let statement = connection.query(queryString, [query.table, query.where[0]], function(error, result) {
+            callback(error, result);
+        });
+        if (query.debug){
+            console.log(statement.sql);
+        }
+    },
+    query: function(queryString, queryArray, callback) {
+        connection.query(queryString, queryArray, function(error, result) {
+            callback(error, result);
+        });
+    },
+
     }
-}
+  
+};
+
+module.exports = orm;

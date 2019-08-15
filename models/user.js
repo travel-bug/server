53f18f31bac123d2f7be421d4c6aeca20fd20de4
Mike Z <mzapata2991@gmail.com>
2019-08-15 18:31:31 -0500

    added user.js file

---
+++ models/user.js
@@ -0,0 +1,35 @@
+let orm = require("./orm");
+
+let user = {
+    insertNewUser: function (user, callback) {
+        userObj.person_email = user.email.toLowerCase();
+        userObj.username = user.username.toLowerCase();
+        userObj.person_password = user.password;
+        userObj.person_firstName = user.firstname;
+        userObj.person_lastName = user.lastname;
+        let query = {
+            table: 'people',
+            data: user
+        };
+        orm.insert(query, callback);
+    }
+
+    // loginUser: function (request, response) {
+    //     orm.select({
+    //         table: 'people',
+    //         column: 'username',
+    //         value: request.body.username
+    //     }, function (error, result) {
+    //         response.json(result);
+
+
+    //     });
+    // }
+
+
+
+};
+
+
+
+module.exports = user;

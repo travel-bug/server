688fc74565818dc578e32090b6ed9de181218358
Mike Z <mzapata2991@gmail.com>
2019-08-15 20:19:59 -0500

    passport talking with user

--- models/user.js
+++ models/user.js
@@ -1,18 +1,23 @@
-let orm = require("./orm");
+let orm = require("../orm");
 
 let user = {
     insertNewUser: function (user, callback) {
+        var userObj = {
+
+        }
         userObj.person_email = user.email.toLowerCase();
         userObj.username = user.username.toLowerCase();
         userObj.person_password = user.password;
-        userObj.person_firstName = user.firstname;
-        userObj.person_lastName = user.lastname;
+        // userObj.person_firstName = user.firstname;
+        // userObj.person_lastName = user.lastname;
         let query = {
             table: 'people',
-            data: user
+            data: userObj
         };
         orm.insert(query, callback);
     }
+
+
 
     // loginUser: function (request, response) {
     //     orm.select({
--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,7 @@ var LocalStrategy = require('passport-local').Strategy;
 module.exports = function (app, passport) {
 
     app.post('/api/signup', passport.authenticate("local-signup"), (req, res)=>{
+    
         console.log(req.user)
         res.json(req.user);
       

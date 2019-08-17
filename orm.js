0c7ba9143b63910e478832f10793ed153a2c6920
Chris Burke <burke1791@gmail.com>
2019-08-17 06:48:14 -0500

    added a join builder function

--- orm.js
+++ orm.js
@@ -5,6 +5,9 @@ let orm = {
     select: function(query, callback) {
         let queryString = "SELECT ?? FROM ??";
         let searchCriteria = [query.columns || ['*'], query.table];
+        if (query.join) {
+            queryString = orm._buildJoinStatement(query, queryString, searchCriteria);
+        }
         if (query.where){
             queryString = orm._buildWhereStatement(query, queryString, searchCriteria);
         }
@@ -26,6 +29,18 @@ let orm = {
         }
         let operator = query.operator || 'AND';
         queryString += whereString.join(operator);
+        return queryString;
+    },
+    _buildJoinStatement: function(query, queryString, searchCriteria) {
+        for (let i in query.join) {
+            queryString += ' JOIN ?? ON ';
+            searchCriteria.push(query.join[i].table)
+            for (let j in query.join[i].condition) {
+                queryString += '?? = ??';
+                searchCriteria.push(query.join[i].condition[j].left);
+                searchCriteria.push(query.join[i].condition[j].right);
+            }
+        }
         return queryString;
     },
     insert: function(query, callback) {

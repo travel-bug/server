bda430f4e3f7391a0eb795a0f7b29795881e1c0f
Mike Z <mzapata2991@gmail.com>
2019-08-20 19:34:31 -0500

    edited doStuff

--- models/doStuff.js
+++ models/doStuff.js
@@ -3,11 +3,53 @@ const orm = require('../orm');
 let doStuff = {
     selectTopPostsDo: function (callback) {
         let query = {
-            table: 'post'
+            table: 'post',
+            data: {},
+            columns: ['post.post_id', 'post.user_id', 'post.location_id', 'post.pics_id', 'post.category', 'people.username', 'pics.pics_url', 'place.place_name'],
+            join: [
+                {
+                    table: 'people',
+                    condition: [
+                        {
+                            left: 'post.user_id',
+                            right: 'people.person_id'
+                        }
+                    ]
+                },
+                {
+                    table: 'pics',
+                    condition: [
+                        {
+                            left: 'post.pics_id',
+                            right: 'post.pics_id'
+                        }
+                    ]
+                },
+                {
+                    table: 'place',
+                    condition: [
+                        {
+                            left: 'post.location_id',
+                            right: 'place.place_id'
+                        }
+                    ]
+                }
+            ],
+            where: [
+                { 'post.category': 'do' }
+            ]
         };
         orm.select(query, callback);
-    }
-    
+    },
+    // searchDoStuff: function(callback) {
+    //     let query = {
+    //         table: 'place',
+    //         data: {},
+    //         columns: ['place.place_id', 'place.place.name', 'place.location_id', 'place.gps_id', 'place.lat_long'],
+
+    //     }
+    // }
+
 }
 
 module.exports = doStuff;

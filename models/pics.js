923b735d1b76dc44d37f2fe677eb935d48234cdc
Chris Burke <burke1791@gmail.com>
2019-08-22 19:55:32 -0500

    added functions to update a user's prof pic

--- models/pics.js
+++ models/pics.js
@@ -7,6 +7,30 @@ let pics = {
       data: pic
     };
     orm.insert(query, callback);
+  },
+
+  updateProfPic: function(pic, callback) {
+    let query = {
+      table: 'pics',
+      data: { 'pic_type': 'old_prof' },
+      where: [
+        {
+          'pics.person_id': pic.person_id
+        },
+        {
+          'pics.pic_type': 'prof'
+        }
+      ]
+    };
+    orm.update(query, callback);
+  },
+
+  setNewProfPic: function(pic, callback) {
+    let query = {
+      table: 'pics',
+      data: pic
+    };
+    orm.insert(query, callback);
   }
 }
 
